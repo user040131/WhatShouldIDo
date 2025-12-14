aab6fbf11b1a7e04a4e27d70ab7ce3a988f6bc18
user040131 <seungjub270@gmail.com>
2025-12-15 01:01:17 +0900

    version 4.0

---
+++ src/main/java/seungjub270/whatshouldido/WhatShouldIDoApplication.java
@@ -0,0 +1,13 @@
+package seungjub270.whatshouldido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldIDoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatShouldIDoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/seungjub270/whatshouldido/WhatShouldIDoApplicationTests.java
@@ -0,0 +1,13 @@
+package seungjub270.whatshouldido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatShouldIDoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
